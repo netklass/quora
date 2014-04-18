7653366b9365d5c2dd7cf7346a17e1689921e7fb
NeverMin <never.min@gmail.com>
2014-04-18 18:21:50 +0800

    fixed repo

--- diff.rb
+++ diff.rb
@@ -11,7 +11,7 @@ end
 t1 = "This is the forst time to useing diff. 魅力的成都\n如果说这里可以活成功."
 t2 = "This is my first time to using diff. 美丽的成都\n如果说这里可以或成功。"
 
-f = open("/home/jason/Desktop/a.html","w+")
+f = open("/tmp/a.html","w+")
 f.puts Diff.diff(t1,t2)
 f.close
 
